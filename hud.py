8f4a3ea20e6811147bb45efc0a2f5f2523ab46d1
viper-not-python <amg2004@outlook.com>
2022-12-07 17:56:52 +0100

    11

--- hud.py
+++ hud.py
@@ -222,6 +222,7 @@ while True:
         c2 = xyz[c1 + 1:].find(",")
         #try:
         x = float(xyz[0:c1])
+        print(xyz[c1 + 1:c2-1])
         y = float(xyz[c1 + 1:c2-1])
         z = float(xyz[c2 + 1:])
         draw_line(200, 200, y, 100, 1, 0, 255, 0)
