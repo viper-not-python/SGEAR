da27040fed3d0c255653d0a9f69b9c614712d09a
viper-not-python <amg2004@outlook.com>
2022-12-26 16:47:26 +0100

    changed ip

--- hud.py
+++ hud.py
@@ -200,7 +200,7 @@ def socket_initialize():
     # Socket Create
     server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     host_name  = socket.gethostname()
-    host_ip = '192.168.170.36'
+    host_ip = '192.168.170.208'
 
     port = 9999
     socket_address = (host_ip,port)
