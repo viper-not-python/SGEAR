1edc942c4450a57a92b363743bdd6bac7acc75c0
viper-not-python <amg2004@outlook.com>
2022-12-26 17:58:35 +0100

    int()

--- hud.py
+++ hud.py
@@ -241,7 +241,7 @@ def check_internet():
 
 def fps_c(fps):
     fps_change = abs(fps - 10)
-    fps_change = 0.5 * math.pow(fps_change, 2)
+    fps_change = int(0.5 * math.pow(fps_change, 2))
     print(fps_change) 
 
 
