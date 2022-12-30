e2f0f8743469186d58a911a3e571b6c8ecf52982
viper-not-python <amg2004@outlook.com>
2022-12-30 13:46:36 +0100

    serial as thread

--- hud.py
+++ hud.py
@@ -13,6 +13,7 @@ import pickle
 import imutils
 from threading import Thread
 from pythonping import ping
+import serial
 
 #python setup
 
@@ -71,6 +72,8 @@ starting_vpn = False
 sending = False
 
 w_custom = 500
+
+ser = False
 
 
 #methods
@@ -319,6 +322,56 @@ def reconnect_wifi():
     time.sleep(1)
     starting_vpn = False
 
+def serial_read():
+    global ser
+    try:
+        SerialIn = serial.Serial("/dev/ttyUSB0",9600)
+        ser = True
+        print("ser = true")
+    except Exception as e:
+        ser = False
+        print("ser = false")
+        print(e)
+
+    def get_data(type_):
+        data = SerialIn.readline()
+        data = data.decode()
+    
+        if type_ == "str":
+            return data
+        if type_ == "int":
+            data = int(data)
+            return data
+
+    def try_voltage():
+        voltage = get_data("str")[0:4]
+        voltage = voltage[0:2] + "." + voltage [2:4]
+        battery_status = voltage + " V"
+        with open("serial/voltage.txt", "w") as v:    #battery_voltage
+            v.write(battery_status)
+
+    def try_distance():
+        dta = get_data("str")
+        i1 = dta.find("u") + 1
+        i2 = dta.find("x")
+        distance_str = dta[i1:i2]
+        distance = int(distance_str)
+        distance_str = distance_str + "cm"
+        if distance == 0:
+            distance_str = "error"
+        with open("serial/distance.txt", "w") as d:    #distance
+            d.write(distance_str)
+
+
+    while True:
+        try:
+            try_voltage()            
+            try_distance()
+            time.sleep(0.01)
+        except:
+            ser = False
+            break
+
 Thread(target=vpn_active).start()
 
 while True:
@@ -375,6 +428,9 @@ while True:
     cv2.putText(frame, fps_str, (225,24), cv2.FONT_HERSHEY_SIMPLEX, fontsize, (0, 255, 0), 1)
     
     a = datetime.datetime.now()
+
+    if ser == False:    
+        Thread(target=serial_read).start()
 
     battery_status = get_battery()
     distance = get_distance()
