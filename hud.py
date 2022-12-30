19ae5c27b9a014c998fd7b00ea27c48127d22a52
viper-not-python <amg2004@outlook.com>
2022-12-30 01:03:52 +0100

    7

--- hud.py
+++ hud.py
@@ -284,7 +284,7 @@ def reconnect_wifi():
     os.system("rfkill block wifi")
     time.sleep(0.5)
     os.system("rfkill unblock wifi")
-    
+    time.sleep(8)    
 
 while True:
     ret, frame = stream.read()
@@ -383,7 +383,7 @@ while True:
             try_connection()
 
     if vpn_active() != 0:
-        #Thread(target=reconnect_wifi).start()
+        Thread(target=reconnect_wifi).start()
         print("vpn down")
     else:
         print("vpn up")
